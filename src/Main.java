b3ef9d46f0a1da10aeeb79a0326dd092b215ea45
Maria Schevtsova <hatulmadan5002@gmail.com>
2022-09-29 17:18:58 +0300

    lambda

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,16 @@ public class Main {
         persons.add(new Person("Антуан","де Сент-Экзюпери", 40));
         persons.add(new Person("Дени", "Дидро", 30));
         persons.add(new Person("Геральт", "из Ривии", 35));
-        Collections.sort(persons, new PersonComparator(2));
-        System.out.println(persons);
+        Comparator<Person> personComparatorSurnameNameAge3 = (p1, p2) -> {
+            if (Math.min(p1.getSurname().split(" ").length, 3) != (Math.min(p2.getSurname().split(" ").length, 3))) {
+                return Integer.compare(p1.getSurname().split(" ").length, p2.getSurname().split(" ").length);
+            }
+            return Integer.compare(p1.getAge(), p2.getAge());
+        };
+        Collections.sort(persons, personComparatorSurnameNameAge3);
+        for (Person person : persons) {
+            System.out.println(person);
+        }
+
     }
 }
